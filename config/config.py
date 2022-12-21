0858b2be0a3bf681d99eb11c6183ffc6b48151e4
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-12-21 16:43:09 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -13,7 +13,7 @@ API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001572595035"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", " ˹ʟᴇɢᴇɴᴅ ✘ ᴍᴜsɪᴄ˼")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "5048100752").split()))
