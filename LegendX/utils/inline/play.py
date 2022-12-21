b5a733e236a4a8723c078e0d3b50dc8edc0efb37
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-12-21 20:04:57 +0530

    Update play.py

--- LegendX/utils/inline/play.py
+++ LegendX/utils/inline/play.py
@@ -296,6 +296,11 @@ def queue_markup(_, videoid, chat_id):
             ),
             InlineKeyboardButton(
                 text="▢", callback_data=f"ADMIN Stop|{chat_id}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="✯ ᴄʟᴏsᴇ ✯", callback_data="close"
             )
         ],
     ]
